23f11ce9e3337ee5cd77d7f85479ac38f8f593c4
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2026-02-09 17:24:02 +0100

    fix : (docker setup) sqlalchemy connection string was broken

--- docker/config/superset/Preconfig.py
+++ docker/config/superset/Preconfig.py
@@ -8,4 +8,4 @@
 import os
 
 REDIS_BASE_URL=f'redis://{os.getenv("REDIS_HOST", "superset_redis")}:{os.getenv("REDIS_PORT", 6379)}'
-SQLALCHEMY_DATABASE_URI='postgresql://{os.getenv("DATABASE_USER"}:{os.getenv("DATABASE_PASSWORD"}@{os.getenv("DATABASE_HOST"}/{os.getenv("DATABASE_DB"}?options=-c%20search_path=superset'
+SQLALCHEMY_DATABASE_URI=f'postgresql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOST")}/{os.getenv("DATABASE_DB")}?options=-c%20search_path=superset'
